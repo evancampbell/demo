a23b076d2acb7fa1c5fd8e5a253e05091d5295c6
Evan <evan.campbell.t@gmail.com>
2010-06-25 04:04:23 -0500

    Done with the demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :microposts
+
+  map.resources :users
+  map.resources :microposts
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
